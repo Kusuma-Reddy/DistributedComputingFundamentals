bad7ccc81f34cb150986ef2e8267246d2bae038d
Kusuma-Reddy <55137538+Kusuma-Reddy@users.noreply.github.com>
2019-09-15 13:08:12 +0530

    Update docker_script.sh

--- Week2/docker_script.sh
+++ Week2/docker_script.sh
@@ -2,23 +2,23 @@
 docker pull nginx
 
 #running the nginx image and assigning a name to the container
-docker run -it -p 80:80 --name nginx1 nginx
+docker run --name nginx1 -P -d nginx
 
 #OR, to run in detached mode
-docker run -d -p 80:80 --name nginx1 nginx
+docker run -d -p 80:80 --name mynginx1 nginx
 
 #optional command: curl localhost:80 in a separate terminal
 
 #open a separate terminal tab and run these commands
 
 #start the container, if needed
-docker start nginx1
+docker start mynginx1
 
 #docker ps shows a list of containers running
 docker ps
 
 #to log into a container
-docker exec -it nginx1 bash
+docker exec -it mynginx1 bash
 
 #change to correct path
 cd usr/share/nginx/html/
@@ -26,8 +26,12 @@ cd usr/share/nginx/html/
 #view the default index.html - this is displayed in your browser once NGINX is succesfully up and running
 cat index.html
 
-#exit the container
-exit
+
+#to append the default html code
+cat >index.html
+#open your web browser
+0.0.0.0
+
 
 
 
